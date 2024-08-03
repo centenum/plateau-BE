1713e9aa45ff4430cf86c957b395c84ab6e49d45
KeneNwogu <nwogujoseph1111@gmail.com>
2024-08-03 18:09:58 +0100

    fix: add swag_from to endpoint

--- routes_general_data.py
+++ routes_general_data.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from config import db
+from flasgger import swag_from
 import json
 
 routes_general_data = Blueprint('general_data', __name__)
@@ -7,6 +8,24 @@ routes_general_data = Blueprint('general_data', __name__)
 political_parties_collection = db['political_parties']
 
 @routes_general_data.route('/general/political-parties', methods=['GET'])
+@swag_from({
+    'responses': {
+        200: {
+            'description': 'All Political Parties',
+            'examples': {
+                'application/json': {
+                    'message': 'All Political Parties',
+                    'political_parties': [
+                        {
+                            'name': 'Party Name',
+                            'acronymn': 'Party Acronymn',
+                        }
+                    ],
+                }
+            }
+        },
+    }
+})
 def get_political_parties():
     political_parties = list(political_parties_collection.find(
         projection={'_id': False}))
