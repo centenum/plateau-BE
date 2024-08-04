12ec7e4dcf5aaf32cee922cbebf578a42fce8ec1
MyEazipay <tech@myeazipay.com>
2024-08-04 09:41:36 +0100

    feat: /get_voter_data encrypted

--- routes_general_data.py
+++ routes_general_data.py
@@ -1,11 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from config import db
 from flasgger import swag_from
-import json
+import json, os
+from cryptography.fernet import Fernet
 
 routes_general_data = Blueprint('general_data', __name__)
 
 political_parties_collection = db['political_parties']
+
+# Encryption key (should be kept secure and consistent)
+encryption_key = os.getenv('ENCRYPTION_KEY_VOTER_DATA')
+encryption_key = encryption_key.encode()
+cipher_suite = Fernet(encryption_key)
 
 @routes_general_data.route('/general/political-parties', methods=['GET'])
 @swag_from({
@@ -40,3 +46,26 @@ def get_political_parties():
         return jsonify({"political_parties": list(political_parties)})
     
     return jsonify({"political_parties": list(political_parties)})
+
+
+@swag_from('swagger_config.yml')
+@routes_general_data.route('/get_voter_data', methods=['GET'])
+def get_voters():
+    polling_unit = request.args.get('polling_unit')
+    ward = request.args.get('ward')
+    lga = request.args.get('lga')
+
+    # Fetch data from MongoDB
+    query = {
+        "polling_unit": polling_unit,
+        "ward": ward,
+        "lga": lga
+    }
+    results = list(db.voters.find(query, {"_id": 0}))  # Exclude the MongoDB _id field
+
+    # Convert results to JSON string and then encrypt
+    results_json = json.dumps(results)
+    encrypted_results = cipher_suite.encrypt(results_json.encode())
+
+    # Return encrypted data as a response
+    return jsonify({"data": encrypted_results.decode()})
