fa9d48f28e06d2e212086cbf38d7bc3cddfd69d9
KeneNwogu <nwogujoseph1111@gmail.com>
2024-08-04 11:21:54 +0100

    fix: add authentication

--- decorators.py
+++ decorators.py
@@ -2,6 +2,9 @@ from functools import wraps
 
 from flask import jsonify, request
 from marshmallow import ValidationError
+from config import db
+
+auth_collection = db.auth
 
 def validate_schema(schema):
     def decorator(f):
@@ -19,3 +22,25 @@ def validate_schema(schema):
             return f(*args, **kwargs)
         return decorated_function
     return decorator
+
+
+def login_required(f):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if 'Authorization' not in request.headers:
+                return jsonify({"message": "Unauthorized access"}), 401
+            
+            auth_header = request.headers.get('Authorization')
+            if not auth_header:
+                return jsonify({"message": "Unauthorized access"}), 401
+            
+            authorized_user = auth_collection.find_one({"token": auth_header})
+            if not authorized_user:
+                return jsonify({"message": "Unauthorized access"}), 401
+            # add the user to the request object
+            request.user = authorized_user
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+        
